3aebc3a5cd0bac769e4d967fd824ac2428b6ed5d
511baoping <64813437@qq.com>
2026-09-04 22:16:19 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+/* 珠珠能量宝瓶 · 离线缓存 */
+const CACHE = 'zzb-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app-core.js',
+  './app-pages.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon.svg'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
